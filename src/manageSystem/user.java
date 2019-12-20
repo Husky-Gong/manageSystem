b191acb7e0cceafe716b0146f49ba192a4f0929b
ZexiGong <gong.ze@husky.neu.edu>
2019-12-20 19:20:42 +0800

    update user object definition

---
+++ src/manageSystem/user.java
@@ -0,0 +1,91 @@
+package manageSystem;
+
+public class user {
+	private int id;
+	private String userName;
+	private String passWord;
+	private int userAge;
+	
+	public user(int id, String userName, String passWord, int userAge) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.passWord = passWord;
+		this.userAge = userAge;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public int getUserAge() {
+		return userAge;
+	}
+
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+
+	@Override
+	public String toString() {
+		return "user [id=" + id + ", userName=" + userName + ", passWord=" + passWord + ", userAge=" + userAge + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((passWord == null) ? 0 : passWord.hashCode());
+		result = prime * result + userAge;
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		user other = (user) obj;
+		if (id != other.id)
+			return false;
+		if (passWord == null) {
+			if (other.passWord != null)
+				return false;
+		} else if (!passWord.equals(other.passWord))
+			return false;
+		if (userAge != other.userAge)
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+}
